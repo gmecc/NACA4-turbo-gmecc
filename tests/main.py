a9afd69a442e82294986adeb6125ae8256e6c8de
SergeyMtt <sb68595@gmail.com>
2024-08-11 12:54:17 +0200

    change main

--- tests/main.py
+++ tests/main.py
@@ -1,16 +1,19 @@
 from naca4turbo import NACA4turbo
-pr = NACA4turbo(p=4, t=10)
-pr.profile(m=10)
-pr.plot()
 
-# расчет профиля по заданному углу поворота потока
-pr.flowAngle(dalpha=64)
-pr.plot()
+def main():
+    pr = NACA4turbo(p=4, t=10)
+    pr.profile(m=10)
+    pr.plot()
 
+    # расчет профиля по заданному углу поворота потока
+    pr.flowAngle(dalpha=64)
+    pr.plot()
 
-# координата верхней поверхности профиля
-print([pr.f.xU, pr.f.yU])
+    # координата верхней поверхности профиля
+    print([pr.f.xU, pr.f.yU])
 
-# координата нижней поверхности профиля
-print([pr.f.xL, pr.f.yL])
+    # координата нижней поверхности профиля
+    print([pr.f.xL, pr.f.yL])
 
+if __name__ == '__main__':
+    main()
--- tests/main_NACA4camb.py
+++ tests/main_NACA4camb.py
@@ -1,4 +1,10 @@
 from naca4turbo import NACA4camb
-pr = NACA4camb('9410')
-pr.plot()
 
+def main():
+    pr = NACA4camb('9410')
+    pr.plot()
+
+
+
+if __name__ == '__main__':
+    main()
